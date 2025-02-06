1d58cc25faddf1151e957d908ee30a5636884ddc
rafaelsndice <rafaelmagnum90@hotmail.com>
2025-02-06 12:53:17 -0300

    tabeela de listas

--- create_table.sql
+++ create_table.sql
@@ -7,3 +7,12 @@ senha varbinary(255)not null,
 primary key(id)
 );
 
+CREATE TABLE listas(
+id int not null auto_increment,
+titulo varchar(256) not null,
+tarefa varchar(256) not null,
+status varchar(50) not null,
+ativo boolean not null,
+primary key(id)
+);
+
